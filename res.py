b84d1f87047f327956783d285d3a68f2239fdb7b
vedamshpawar <vedamshpawar@gmail.com>
2026-01-25 12:11:18 +0530

    practiced a HTTP methods today

--- res.py
+++ res.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -6,16 +6,23 @@ app = Flask(__name__)
 def home():
     return render_template("base.html", name="Admin")
 
-@app.route("/<name>")
-def user(name):
-    return f'Hello {name}'
 
 @app.route("/admin")
 def admin():
     return redirect(url_for("user", name="Admin!"))
 
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
 
-    
+@app.route("/<usr>")
+def user(usr):
+    return f'<h1>{usr}</h1>'
+
 
 
 
